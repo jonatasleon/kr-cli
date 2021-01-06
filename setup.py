e892e0cf205160758d157cf62a3e6d14dd45281b
Jonatas Leon <jonatas.leon@gmail.com>
2021-01-06 00:51:47 -0300

    Improves setup.py definition

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
+import pathlib
 from setuptools import setup
 
+from kr import __version__
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
 setup(
-    name='kr-cli',
-    version='0.1',
-    py_modules=['kr'],
-    install_requires=[
-        'Click',
-        'beautifulsoup4',
-        'requests',
-        'tabulate',
+    name="kr-cli",
+    version=__version__,
+    description="CLI tool to search and download roms from Killers Roms",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonatasleon/kr-cli",
+    author="Jonatas Leon",
+    author_email="j@jonatasleon.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-    entry_points='''
+    packages=["kr"],
+    install_requires=[
+        "Click",
+        "beautifulsoup4",
+        "requests",
+        "tabulate",
+    ],
+    entry_points="""
         [console_scripts]
         kr=kr:cli
-    ''',
+    """,
 )
-
